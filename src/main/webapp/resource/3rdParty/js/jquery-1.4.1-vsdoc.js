6797441c9089b8b70d56049c00876179c2c0c9c4
lgh <106517651@qq.com>
2015-11-28 15:29:17 +0800

    搭建项目

--- src/main/webapp/resource/3rdParty/js/jquery-1.4.1-vsdoc.js
+++ src/main/webapp/resource/3rdParty/js/jquery-1.4.1-vsdoc.js
@@ -3923,7 +3923,7 @@ var Expr = Sizzle.selectors = {
 				match[3] = test[3] - 0;
 			}
 
-			// TODO: Move to normal caching system
+			// TODO: Move to normal caching api
 			match[0] = done++;
 
 			return match;
