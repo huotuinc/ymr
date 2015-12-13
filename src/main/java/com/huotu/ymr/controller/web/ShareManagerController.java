85d43641dc58681c29ee7960dc46c70c984032b4
shiliting <861819089@qq.com>
2015-12-13 16:59:45 +0800

    前台爱分享

--- src/main/java/com/huotu/ymr/controller/web/ShareManagerController.java
+++ src/main/java/com/huotu/ymr/controller/web/ShareManagerController.java
@@ -3,17 +3,24 @@ package com.huotu.ymr.controller.web;
 import com.huotu.ymr.common.CommonEnum;
 import com.huotu.ymr.common.EnumHelper;
 import com.huotu.ymr.entity.Share;
-import com.huotu.ymr.model.searchCondition.ShareSearchModel;
+import com.huotu.ymr.model.ResultModel;
 import com.huotu.ymr.model.backend.share.BackendShareModel;
+import com.huotu.ymr.model.searchCondition.ShareSearchModel;
 import com.huotu.ymr.service.ShareService;
+import com.huotu.ymr.service.StaticResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Date;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 /**
  * Created by lgh on 2015/12/3.
@@ -24,6 +31,9 @@ public class ShareManagerController {
     public static final int PAGE_SIZE=20;
     @Autowired
     ShareService shareService;
+
+    @Autowired
+    StaticResourceService staticResourceService;
 
     @RequestMapping(value = "/showBottomGeneralize",method = RequestMethod.GET)
     public String showBottomGeneralize() throws Exception{
@@ -63,7 +73,22 @@ public class ShareManagerController {
     public String getYmrShareList(ShareSearchModel shareSearchModel,Model model) throws Exception {
         //todo 用户权限方面的操作
         Page<Share> shares=shareService.findPcShareList(shareSearchModel);
-        model.addAttribute("allShareList", shares);//文章列表
+        List<BackendShareModel> backendShareModels=new ArrayList<>();
+        for(Share s:shares){
+            BackendShareModel backendShareModel=new BackendShareModel();
+            backendShareModel.setShareTitle(s.getTitle());
+            backendShareModel.setId(s.getId());
+            backendShareModel.setTop(s.getTop());
+            backendShareModel.setTime(s.getTime());
+            backendShareModel.setUserType(s.getOwnerType().getName());
+            backendShareModel.setPraiseQuantity(s.getPraiseQuantity());
+            backendShareModel.setRelayQuantity(s.getRelayQuantity());
+            backendShareModel.setView(s.getView());
+            backendShareModel.setCheckType(s.getCheckStatus().getName());
+            backendShareModels.add(backendShareModel);
+        }
+        model.addAttribute("allShareList", backendShareModels);//文章列表model
+        model.addAttribute("pageNo",shareSearchModel.getPageNoStr());//当前页数
         model.addAttribute("totalPages",shares.getTotalPages());//总页数
         model.addAttribute("totalRecords", shares.getTotalElements());//总记录数
         return "manager/share/ymrShareList";
@@ -101,15 +126,17 @@ public class ShareManagerController {
      *
      * @return
      */
-    @RequestMapping(value = "saveShare",method = RequestMethod.POST)
+    @RequestMapping(value = "/saveShare",method = RequestMethod.POST)
     public String saveShare(BackendShareModel backendShareModel) throws Exception{
+
         Share share=new Share();
+        share.setName("官方");
         share.setCheckStatus(CommonEnum.CheckType.audit);
         share.setOwnerId(0L);//todo 当前登录的用户
         share.setScore(0);
-        share.setTitle(backendShareModel.getTitle());
+        share.setTitle(backendShareModel.getShareTitle());
         share.setLinkUrl("");//todo
-        share.setContent(backendShareModel.getContent());
+        share.setContent(backendShareModel.getShareContent());
         share.setImg("");
         share.setIntro("");//todo
         share.setOwnerType(CommonEnum.UserType.official);
@@ -128,5 +155,74 @@ public class ShareManagerController {
         share.setRelayReward(0);
         shareService.addShare(share);
         return "redirect:getYmrShareList";
+    }
+
+
+    /**
+     * 帖子的通过与不通过
+     * @param shareId   帖子ID
+     * @param type      0：不通过，1：通过
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/passOrNot",method = RequestMethod.POST)
+    @ResponseBody
+    public ResultModel pass(Long shareId,Long type) throws Exception{
+        ResultModel resultModel=new ResultModel();
+        if(shareId==null){
+            resultModel.setCode(0);
+            resultModel.setMessage("参数异常");
+            return resultModel;
+        }
+        Share share=shareService.findOneShare(shareId);
+        if(Objects.isNull(share)){
+            resultModel.setCode(0);
+            resultModel.setMessage("没找到该帖子，或许已被删除!");
+        }
+        if(type==0){
+            share.setCheckStatus(CommonEnum.CheckType.notPass);
+        }else if(type==1){
+            share.setCheckStatus(CommonEnum.CheckType.pass);
+        }
+        share=shareService.addShare(share);
+        resultModel.setCode(1);
+        resultModel.setMessage(share.getCheckStatus().getName());
+        return resultModel;
+    }
+
+
+
+
+
+    /**
+     * 图片上传
+     * @param file
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/UploadImg",method = RequestMethod.POST)
+    @ResponseBody
+    public ResultModel UploadImg(@RequestParam("shareImg") MultipartFile file,HttpServletResponse response) throws Exception{
+        ResultModel resultModel=new ResultModel();
+        //文件格式判断
+        if (ImageIO.read(file.getInputStream()) == null) {
+            resultModel.setCode(0);
+            resultModel.setMessage("请上传图片文件！");
+            return resultModel;
+        }
+        if (file.getSize() == 0) {
+            resultModel.setCode(0);
+            resultModel.setMessage("请上传图片！");
+            return resultModel;
+        }
+        //保存图片
+        String fileName = StaticResourceService.SHSRES_IMG + UUID.randomUUID().toString() + ".png";
+        staticResourceService.uploadResource(fileName, file.getInputStream());
+        response.setHeader("X-frame-Options", "SAMEORIGIN");
+        resultModel.setCode(1);
+        resultModel.setMessage("图片上传成功！");
+        return resultModel;
+
     }
 }
